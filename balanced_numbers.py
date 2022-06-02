afe1b115e8b3aea77b8f7d49d84904a9da45541a
lmh00 <lmh004@protonmail.com>
2022-06-02 11:55:46 +0900

    balanced numbers: https://www.codewars.com/kata/5a4e3782880385ba68000018/train/python

--- balanced_numbers.py
+++ balanced_numbers.py
@@ -1,25 +1,35 @@
 def balanced_num(number):
-    even_list =
-    even_length = 0
-    even_sum_left = 0
-    even_sum_right = 0
+    length = len(str(number))
+    arr = []
+    left = 0
+    right = 0
 
-    if len(str(number)) % 2 > 0:
-        if len(str(number)) == 1:
-            print(f"n = {number}, balanced")
+    for i in str(number):
+        arr.append(int(i))
+
+    for i in range(length):
+        if (i < length // 2):
+            left += arr[i]
         else:
-            even_length = int(len(str(number)) / 2)
-            for i in range(0, even_length):
-                even_sum_left += i
-            print("even_list: ", even_list)
-            print("even_length: ", even_length)
-            print("even_sum_left: ", even_sum_left)
-            print("even_sum_right: ", even_sum_right)
+            right += arr[i]
+
+    if length % 2 > 0:
+        right -= arr[(length // 2)]
     else:
-        print(f"n = {number}, odd")
+        left -= arr[((length // 2) - 1)]
+        right -= arr[(length // 2)]
+
+
+    if length < 3:
+        return "Balanced"
+    else:
+        if left != right:
+            return "Not Balanced"
+        else:
+            return "Balanced"
 
 balanced_num(1)
 balanced_num(22)
-balanced_num(333)
-balanced_num(4444)
-balanced_num(55555)
+balanced_num(233)
+balanced_num(4244)
+balanced_num(52555)
